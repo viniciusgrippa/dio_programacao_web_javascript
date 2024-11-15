592a6df4e83944105589c4c0d8b4cd580a0d8e65
viniciusgrippa <vinicius.grippa@hotmail.com>
2024-11-14 22:17:09 -0300

    Introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Vinícius Grippa";
+var idade = 29;
+var idade2 = 10;
+
+var frase = "Japão é o melhor time do mundo";
+
+// alert(nome + " tem " + idade + " anos.");
+// alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
