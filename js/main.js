91118555a6e58d133b2267fb96bfda1aac86ab51
viniciusgrippa <vinicius.grippa@hotmail.com>
2024-11-15 00:49:43 -0300

    Cria Arrays e Dicionario

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-var nome = "Vinícius Grippa";
-var idade = 29;
-var idade2 = 10;
+var frutas = [{ nome: "maçã", cor: "vermelha" }, { nome: "uva", cor: "roxa" }];
 
-var frase = "Japão é o melhor time do mundo";
+console.log(frutas[1].nome);
+
+// var fruta = { nome: "maçã", cor: "vermelha" }
+
+// console.log(furta.cor);
+
+// var lista = ["maçã", "pera", "laranja"];
+// lista.push("uva");
+// lista.pop();
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" | "));
+
+
+// var nome = "Vinícius Grippa";
+// var idade = 29;
+// var idade2 = 10;
+
+// var frase = "Japão é o melhor time do mundo";
 
 // alert(nome + " tem " + idade + " anos.");
 // alert(idade + idade2);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase);
-console.log(frase.replace("Japão", "Brasil"));
-console.log(frase.toUpperCase());
+// console.log(nome);
+// console.log(idade + idade2);
+// console.log(frase);
+// console.log(frase.replace("Japão", "Brasil"));
+// console.log(frase.toUpperCase());
